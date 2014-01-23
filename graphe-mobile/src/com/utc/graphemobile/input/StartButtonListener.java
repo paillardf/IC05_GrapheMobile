608ab6d6b85b0f6695181b52fbb97715d6200f3e
Florian Paillard <dev@sertook.com>
2014-01-23 12:51:10 +0100

    Correction de l'arret des threads sur Android

--- graphe-mobile/src/com/utc/graphemobile/input/StartButtonListener.java
+++ graphe-mobile/src/com/utc/graphemobile/input/StartButtonListener.java
@@ -32,61 +32,92 @@ public class StartButtonListener extends ActorGestureListener {
 			int button) {
 		String targetName = this.getTouchDownTarget().getName();
 		System.out.println(targetName);
+
 		if (targetName == null || screen.getGraph() == null) {
 			System.out.println("NULL");
 			return;
 		}
+
 		if (targetName.equals("start")) {
 			if (getSpatialization() != null && getSpatialization().isAlive()) {
 				return;
 			}
-			
+
 			System.out.println(getList().getSelection());
-			
-			if(getList().getSelection().equals("Abstract Force")) {
+
+			if (getList().getSelection().equals("Abstract Force")) {
 				layout = (Layout) new ForceAtlasLayout();
-			} else if(getList().getSelection().equals("FruchtermanReingold")) {
+			} else if (getList().getSelection().equals("FruchtermanReingold")) {
 				layout = (Layout) new FruchtermanReingold();
-			}  else if(getList().getSelection().equals("MultiLevel")) {
-				layout = (Layout) new MultiLevelLayout(new MaximalMatchingCoarsening());
-			} else if(getList().getSelection().equals("Random")) {
+			} else if (getList().getSelection().equals("MultiLevel")) {
+				layout = (Layout) new MultiLevelLayout(
+						new MaximalMatchingCoarsening());
+			} else if (getList().getSelection().equals("Random")) {
 				layout = (Layout) new RandomLayout(433);
-			} else if(getList().getSelection().equals("Rotate")) {
+			} else if (getList().getSelection().equals("Rotate")) {
 				layout = (Layout) new RotateLayout(90);
-			} else if(getList().getSelection().equals("Scale")) {
-				//layout = (Layout) new ScaleLayout(45);
-			} else if(getList().getSelection().equals("Yifan Hu")) {
+			} else if (getList().getSelection().equals("Scale")) {
+				// layout = (Layout) new ScaleLayout(45);
+			} else if (getList().getSelection().equals("Yifan Hu")) {
 				layout = (Layout) new YifanHuLayout(new StepDisplacement(1f));
 			}
-				
-			Thread spatialization = new Thread(new Runnable() {
 
-				@Override
-				public void run() {
-					AutoLayout autoLayout = new AutoLayout(60, TimeUnit.SECONDS);
-					autoLayout.setGraphModel(screen.getGraph().getGraphModel());
-					autoLayout.addLayout(layout, 1.0f);
-					autoLayout.execute();
-				}
 
-			});
-			screen.getUIStage().getRightMenuSpatialization().setSpatialization(spatialization);
+			Thread spatialization = new SpatializationThread(
+					new SpatializationRunnable());
+
+			screen.getUIStage().getRightMenuSpatialization()
+					.setSpatialization(spatialization);
 			getSpatialization().start();
 		} else if (targetName.equals("stop")) {
-			if(getSpatialization() == null){
+			if (getSpatialization() == null) {
 				return;
 			}
-			getSpatialization().stop();
-			screen.getUIStage().getRightMenuSpatialization().setSpatialization(null);
+			((SpatializationThread)getSpatialization()).finish();
+			screen.getUIStage().getRightMenuSpatialization()
+					.setSpatialization(null);
 		}
 	}
-	
+
 	private Thread getSpatialization() {
-		return screen.getUIStage().getRightMenuSpatialization().getSpatialization();
+		return screen.getUIStage().getRightMenuSpatialization()
+				.getSpatialization();
 	}
-	
+
 	private List getList() {
 		return screen.getUIStage().getRightMenuSpatialization().getList();
+	}
+
+	public class SpatializationRunnable implements Runnable {
+
+		public AutoLayout autoLayout;
+
+		public SpatializationRunnable() {
+			autoLayout = new AutoLayout(20, TimeUnit.SECONDS);
+			autoLayout.setGraphModel(screen.getGraph().getGraphModel());
+			autoLayout.addLayout(layout, 1.0f);
+
+		}
+
+		@Override
+		public void run() {
+			autoLayout.execute();
+
+		}
+	}
+
+	public class SpatializationThread extends Thread {
+
+		private SpatializationRunnable r;
+
+		public SpatializationThread(SpatializationRunnable r) {
+			super(r);
+			this.r = r;
+		}
+
+		public void finish() {
+			r.autoLayout.cancel();
+		}
 	}
 
 }
